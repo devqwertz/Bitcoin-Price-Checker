e8bb0ab2dbb5b99acf658c3ddf56ab028b2e2fec
DevQwertz <jezcapcut@gmail.com>
2026-06-13 16:06:03 +0200

    Created the webView.py file

---
+++ webView.py
@@ -0,0 +1,19 @@
+import time
+import requests
+from plyer import notification
+from flask import Flask
+
+app = Flask(__name__)
+
+def check_crypto():
+    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+    response = requests.get(url).json()
+    return response["price"]
+
+@app.route("/")
+def index():
+    price = check_crypto()
+    return f"Bitcoin is currently at ${price}"
+
+if __name__ == "__main__":
+    app.run()
